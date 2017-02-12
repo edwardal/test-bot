140cd4bf5e4d686f83f5bfefae30b1041de491f6
edwardal <ealmeter2@gmail.com>
2017-02-12 09:55:51 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ function postMessage() {
 
   body = {
     "bot_id" : botID,
-    "text" : botResponse
+    "text" : "Test Response"
   };
 
   console.log('sending ' + botResponse + ' to ' + botID);
