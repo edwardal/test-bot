25a358420277a8d80e2706804ad1cbe9ee06e826
edwardal <ealmeter2@gmail.com>
2017-03-16 02:37:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex1 = "JEff";
+      botRegex1 = "Jeff";
       botRegex2 = "jeff";
       botRegex3 = "JEFF";
 
