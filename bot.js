27203eded09385b62225468aeee6b3af887abe22
edwardal <ealmeter2@gmail.com>
2017-09-26 17:29:31 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,9 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex1 = "jeff";
+      botRegex1 = "/jeff/i";
 
-  if(request.text && request.text != "Who the fuck is Jeff?" && (request.text.indexOf(botRegex1) > -1 ) {
+  if(request.text && request.text != "Who the fuck is Jeff?" && (request.text.indexOf(botRegex1) > -1 )){
     this.res.writeHead(200);
     postMessage();
     this.res.end();
